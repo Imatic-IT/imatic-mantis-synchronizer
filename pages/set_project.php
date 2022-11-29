0ad4998dae0bd4dc9b3029d9217dc266a8487b7c
mates <matejbrodziansky@gmail.com>
2022-11-29 07:38:28 +0100

    Updated redirect in set_project.php

--- pages/set_project.php
+++ pages/set_project.php
@@ -8,6 +8,6 @@ access_ensure_global_level(config_get('manage_plugin_threshold'));
 if ($_POST['project_for_synchronize']) {
 
     plugin_config_set('project_for_synchronize', [$_POST['project_for_synchronize']]);
-    print_header_redirect(plugin_page('select_projects'));
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 }
