a7ce406676636492a48fae53e17cf7fe1cb91b0a
mates <matejbrodziansky@gmail.com>
2022-12-13 18:06:15 +0100

    Removed config - instead of it is a webhook system

--- pages/links.php
+++ pages/links.php
@@ -3,7 +3,6 @@
     <div class="space-10"></div>
     <div class="form-container">
         <a class="btn btn-primary btn-sm" href="<?php echo plugin_page('webhooks') ?>"><img height="20px" width="20px" src="<?php echo plugin_file('icons/icons8-webhook-48.png') ?>" alt=""> Create webhooks</a>
-        <a class="btn btn-primary btn-sm" href="<?php echo plugin_page('config_create') ?>">Create config</a>
         <a class="btn btn-primary btn-sm" href="<?php echo plugin_page('select_projects') ?>">Select project</a>
         <a class="btn btn-primary btn-sm" href="<?php echo plugin_page('synchronizer_logs') ?>">Show logs</a>
     </div>
